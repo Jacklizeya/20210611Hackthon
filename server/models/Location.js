d4fc82a19e5f1d934dcdcc5fbd2c4adcf75fb5d2
Vladimir Zhukov <Zhukovtnv@gmail.com>
2021-06-11 12:57:57 -0600

    Modified Data Acess

--- server/models/Location.js
+++ server/models/Location.js
@@ -3,21 +3,38 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const locationSchema = new Schema({
-  locationName: {
+  locationname: {
     type: String,
     required: true,
     unique: true
   },
-  address: {
-    type: String,
-  },
   coordinates: {
-    type: Object,
-    required: true
-  },
-  type: {
-    type: String
+    type: Object
   }
 });
 
+const locationModel = mongoose.model("swimming", locationSchema, "swimming");
+
+async function getDataByType(type=""){
+  if ( type===""){
+    return locationModel.find({});
+  }else{
+    return locationModel.find({type}).exec();
+  }
+
+
+  return 
+}
+
+async function getTypes()
+{
+    return locationModel.find({},'type' ).distinct('type').exec();
+}
+
 module.exports = mongoose.model('Location', locationSchema, 'location');
+
+module.exports = {
+  locationModel,
+  getDataByType,
+  getTypes
+}
--- server/models/db.js
+++ server/models/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true  });
 const db = mongoose.connection;
 db.once('open', (_) =>
   console.log('MongoDB is now connected:', process.env.MONGODB_URL)
 );
 db.on('error', (err) => console.error('MongoDB connection error!', err));
+//db.getCollectionNames().toArray().then( (l)  => console.log(l));
